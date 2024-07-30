32711cb68cf9cad0f65bb7fbb3278212c9acf411
Avirat201189221 <avirat0603@gmail.com>
2024-07-31 00:51:48 +0530

    Integrated chatbot completely

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import FinBot from './components/finBot/finBot'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <FinBot/>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
