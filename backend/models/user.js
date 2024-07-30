728b069a03e8ef4b53a2a257433eb6aa66dead56
Akshavya2509 <Akshavya2509@gmail.com>
2024-07-31 05:20:29 +0530

    Add timestamps

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,40 +1,37 @@
 const mongoose = require('mongoose');
 
+
 const MutualFund = new mongoose.Schema({
-    mutualFund: { type: String},
-    amount: { type: Number}
-});
+    mutualFund: { type: String },
+    amount: { type: Number }
+}, { timestamps: true });
 
 const IndianStockMarket = new mongoose.Schema({
     companyName: { type: String },
     units: { type: Number },
     amount: { type: Number }
-});
+}, { timestamps: true });
 
 const Fd = new mongoose.Schema({
-    bankName: { type: String},
-    amountInvested: { type: Number},
-    date : {type: Date},
-    tenure: {type: Number},
-    interestRate: {type: Number},
-});
+    bankName: { type: String },
+    amountInvested: { type: Number },
+    tenure: { type: Number },
+    interestRate: { type: Number }
+}, { timestamps: true });
 
 const usShares = new mongoose.Schema({
     companyName: { type: String },
     units: { type: Number },
     amount: { type: Number }
-});
+}, { timestamps: true });
 
 const rbiBonds = new mongoose.Schema({
-    units: { type: Number },
-    date : {type: Date},
-});
+    units: { type: Number }
+}, { timestamps: true });
 
 const sgbBonds = new mongoose.Schema({
-    units: { type: Number },
-    date : {type: Date},
-});
-
+    units: { type: Number }
+}, { timestamps: true });
 
 const UserSchema = new mongoose.Schema({
     phoneNumber: { type: String },
