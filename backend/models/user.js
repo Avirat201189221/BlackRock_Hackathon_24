fe3d9a61443f6dac39390956b728f4207d6c56e3
Akshavya2509 <Akshavya2509@gmail.com>
2024-07-30 22:09:03 +0530

    compressed schemas to single file

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,30 @@
 const mongoose = require('mongoose');
+const IndianStockMarket = require('./indianStockMarket');
+const MutualFund = require('./mutualFund');
+const Fd = require('./fixedDeposit');
 
-const UserSchema = new mongoose.Schema({
-    username: { type: toString},
+const MutualFund = new mongoose.Schema({
+    mutualFund: { type: toString},
     amount: { type: Number}
 });
 
-module.exports = mongoose.model('users', UserSchema);
+const IndianStockMarket = new mongoose.Schema({
+    companyName: { type: String },
+    units: { type: Number },
+    amount: { type: Number }
+});
+
+const Fd = new mongoose.Schema({
+    bankName: { type: String},
+    amount: { type: Number}
+});
+
+const UserSchema = new mongoose.Schema({
+    phoneNumber: { type: String },
+    amount: { type: Number },
+    indianStockMarketInvestment: [{ type: IndianStockMarket }],
+    mutualFundInvestment: [{ type: MutualFund }],
+    fdInvestment: [{ type: Fd }]
+});
+
+module.exports = mongoose.model('User', UserSchema);
